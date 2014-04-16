bad1e34d75f46a13f5c5ef98ee1dba2cf736eca5
northway <laszlo.a@met.hu>
2014-04-16 16:56:50 +0200

    Swiftmail PHP 5.5 fix

--- lib/vendor/swiftmailer/classes/Swift/Transport/StreamBuffer.php
+++ lib/vendor/swiftmailer/classes/Swift/Transport/StreamBuffer.php
@@ -38,6 +38,8 @@ class Swift_Transport_StreamBuffer
   /** Translations performed on data being streamed into the buffer */
   private $_translations = array();
   
+  protected $_sequence;
+
   /**
    * Create a new StreamBuffer using $replacementFactory for transformations.
    * @param Swift_ReplacementFilterFactory $replacementFactory
